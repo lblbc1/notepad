1655cbc2682e1ba7a67c03198fa2d063af60309f
花生皮编程 <309032663@qq.com>
2021-11-10 12:08:59 +0000

    update notepad_flutter/lib/main.dart.

--- notepad_flutter/lib/main.dart
+++ notepad_flutter/lib/main.dart
@@ -1,119 +1,125 @@
 import 'package:flutter/material.dart';
+import 'db_helper.dart';
 
 /// 厦门大学计算机专业 | 前华为工程师
 /// 分享编程技术，没啥深度，但看得懂，适合初学者。
 /// Java | 安卓 | 前端 | Flutter | iOS | 小程序 | 鸿蒙
 /// 公众号：花生皮编程
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: '花生皮编程',
+      home: MyHomePage(title: '花生皮编程'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  BookDatabase bookSqlite = BookDatabase();
+  var name = "";
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  void initState() {
+    super.initState();
+    addData();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+              name,
             ),
+            ElevatedButton(
+              child: Text('写数据'),
+              onPressed: () {
+                addData();
+              },
+            ),
+            ElevatedButton(
+              child: Text('读数据'),
+              onPressed: () {
+                queryData();
+              },
+            ),
+            ElevatedButton(
+              child: Text('改数据'),
+              onPressed: () {
+                modifyData();
+              },
+            ),
+            ElevatedButton(
+              child: Text('删数据'),
+              onPressed: () {
+                deleteData();
+              },
+            )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  void addData() async {
+    await bookSqlite.openSqlite();
+    await bookSqlite.insert(Book(0, "花生皮编程"));
+    await bookSqlite.close();
+    setState(() {
+      name = "已成功写入数据";
+    });
+  }
+
+  void queryData() async {
+    await bookSqlite.openSqlite();
+    Book? book = await bookSqlite.query(0);
+    await bookSqlite.close();
+    setState(() {
+      if (book == null) {
+        name = "数据不存在";
+      } else {
+        name = book.name;
+      }
+    });
+  }
+
+  void modifyData() async {
+    await bookSqlite.openSqlite();
+    Book? book = await bookSqlite.query(0);
+    if (book != null) {
+      book.name = "花生皮编程2";
+      await bookSqlite.update(book);
+      await bookSqlite.close();
+      setState(() {
+        name = book.name;
+      });
+    }
+  }
+
+  void deleteData() async {
+    await bookSqlite.openSqlite();
+    await bookSqlite.delete(0);
+    await bookSqlite.close();
+    setState(() {
+      name = "数据已删除";
+    });
   }
 }
