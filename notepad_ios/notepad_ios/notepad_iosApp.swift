05d010143dabf9b2613da4924defa6626eac36df
jerry <309032663@qq.com>
2021-11-12 19:45:03 +0800

    update

---
+++ notepad_ios/notepad_ios/notepad_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  notepad_iosApp.swift
+//  notepad_ios
+//
+//  Created by jerry on 2021/11/12.
+//
+
+import SwiftUI
+
+@main
+struct notepad_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
