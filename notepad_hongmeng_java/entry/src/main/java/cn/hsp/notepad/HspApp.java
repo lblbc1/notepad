6f71667147a62a65a01c59b979b50709a4f49b45
jerry <309032663@qq.com>
2021-12-26 14:43:22 +0800

    update

--- notepad_hongmeng_java/entry/src/main/java/cn/hsp/notepad/HspApp.java
+++ notepad_hongmeng_java/entry/src/main/java/cn/hsp/notepad/HspApp.java
@@ -9,7 +9,7 @@ import ohos.aafwk.ability.AbilityPackage;
  * 包含：Java | 安卓 | 前端 | Flutter | iOS | 小程序 | 鸿蒙
  * 公众号：花生皮编程
  */
-public class MyApplication extends AbilityPackage {
+public class HspApp extends AbilityPackage {
     @Override
     public void onInitialize() {
         super.onInitialize();
