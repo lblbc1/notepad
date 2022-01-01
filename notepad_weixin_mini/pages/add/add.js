ceb7b8d4f7d0b7a0f6a6b02d33dff490402f3c98
jerry <309032663@qq.com>
2022-01-01 15:52:23 +0800

    update

---
+++ notepad_weixin_mini/pages/add/add.js
@@ -0,0 +1,30 @@
+/**
+ * 厦门大学计算机专业 | 前华为工程师
+ * 专注《零基础学编程系列》https://cxyxy.blog.csdn.net/article/details/121134634
+ * 包含：Java | 安卓 | 前端 | Flutter | iOS | 小程序 | 鸿蒙
+ * 公众号：花生皮编程
+ **/
+var util = require('../../utils/util.js')
+
+Page({
+  data: {
+    content: ""
+  },
+  methods: {
+  },
+  addData(e) {
+    var dataList = wx.getStorageSync('list') || { index: 0, list: [], time: 0 };
+    var value = e.detail.value;
+    if (value.content == '' || value.content == null) {
+      wx.showToast({
+        title: '内容不能为空',
+        icon: 'error'
+      })
+      return
+    }
+    value.id = util.wxuuid();
+    dataList.list.unshift(value);
+    wx.setStorageSync('list', dataList);
+    wx.navigateBack()
+  }
+})
